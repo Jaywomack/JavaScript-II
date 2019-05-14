a999cf73c27e88728bb42c659e9003275f857aca
Jared Womack <womackjw@hotmail.com>
2019-05-14 11:33:32 -0700

    Lecture notes finished

--- practice.js
+++ practice.js
@@ -74,26 +74,153 @@
 /**************************************************
  * Callback code along
  */
-const elements = ['earth', 'wind', 'fire', 'water'];
+// const elements = ['earth', 'wind', 'fire', 'water'];
+// const els = ['pencil', 'notebook', 'backpack', 'textbooks'];
 
-const newForEach = (list, callBack) => {
-    for(let i = 0; i < list.length; i++) {
-        callBack(list[i], i)
-    }
-};
+// const newForEach = (list, callBack) => {
+//     for(let i = 0; i < list.length; i++) {
+//         callBack(list[i], i)
+//     }
+// };
 
-newForEach(elements, function(item, index) {
-    console.log(item, index);
-});
+// newForEach(elements, function(item, index) {
+//     console.log(item, index);
+// });
+
+// newForEach(els, function(item, index) {
+//     console.log(item, index)
+
+// });
+
+// const elements = ['earth', 'wind', 'fire', 'water'];
+// const els = ['pencil', 'notebook', 'backpack', 'textbooks'];
+
+// function showFirst(array, callback) {
+//     callback(array[0])
+// }
+
+// function showLength(list, cb) {
+//     cb(list.length);
+// }
+// showFirst(elements, (firstItem) => {
+//     console.log(firstItem)
+// })
+
+// showLength(elements, (lengthOfList) => {
+//     console.log(lengthOfList)
+// })
+// /*************************************************************************
+//  * 
+//  */
+// // For loop review
+
+// ninjaTurtles = ["Leo", "Ralph", "Donnie", "Mikey"]
+
+//  for(let i = 0; i <ninjaTurtles.length; i++) {
+//     console.log( `${ninjaTurtles[i]} eats ${ninjaTurtles[i+1]}`) 
+// }
+
+// // // write a for loop which returns a string "ninjaTurtleName eats a slice of pizza." for each Teenaged Mutant Ninja Turtle in the array.
+// // for(let i = 0; i < ninjaTurtles.length; i++) {
+// //     console.log(`${ninjaTurtles[i]} eats a slice of pizza.`)
+// // }
+// /*************************************
+//  * Lecture 
+//  * Closure
+//  */
+// function kingdom () {
+//     const queen = 'Queen Hannah';
+//     console.log(`Long live the ${queen}`)
+
+//     //a city in the kingdom
+//     function city() {
+//         const mayor = 'Mayor Eli';
+//         console.log(`${queen} demands more taxes!`);
+
+//         function town() {
+//             const townCrier = 'Crier Austen'
+//             console.log(`${townCrier} has a message for you`)
+//             town()
+//         }
+//     }
+//     city()
+// }
+
+// kingdom()
 
 
+/***************************************************************
+ * Lecture
+ * Callbacks
+ */
+
+//  function compute(param1, param2, callback) {
+//      return callback(param1, param2);           //this is a callback function that will take
+//                                                 //in 2 parameters and then pass call the function as  the callback for the third 
+//  }
+
+//  function add (a, b) {
+//      return a + b                             
+//  }
+
+// function subtract (a, b) {
+//     return a - b
+// }
+
+// function multiply (a, b) {
+//     return a * b
+// }
+
+// console.log(compute(2, 4, add));
+// console.log(compute(4, 4, multiply));
+// console.log(compute(3, 3, subtract));
+
+//Async Callback Example
+// setTimeout(function() {
+//     console.log('Hello');
+// }, 3000);
 
 
+/***************************************************************
+ * Lecture 
+ * Advanced Array Methods
+ */
+const cityData = [
+    {"city":"seattle", "state":"WA", "population":652405, "land_area":83.9},
+    {"city":"new york", "state":"NY", "population":8405837, "land_area":302.6},
+    {"city":"boston", "state":"MA", "population":645966, "land_area":48.3},
+    {"city":"kansas city", "state":"MO", "population":467007, "land_area":315}
+  ];
+
+// for(let i = 0; i < cityData.length; i++) {
+//     function doSomething() {
+//         console.log(`My city: ${cityData[i].city}`);
+//     }
+//     doSomething();           //A for loop allows to iterate over an array by two or threes or in reverse. the for each goes
+                                //over each item in the array
+// }
 
 
+// cityData.forEach( function(currentCity) {
+//     console.log(`My city: ${currentCity.city}`) //A forEach loop that iterates through every item in an array
+// });                                              //and returns whatever is done in the function
 
+let cityPopulations = cityData.map( function(iteratedCity) {
+    return iteratedCity.population                // The .map array method will iterate over the array and 
+})
+console.log(cityPopulations)
 
+let bigCities = cityData.filter( function (landMass) {
+    return landMass.land_area > 200;
+})
 
+console.log(bigCities)
+
+let totalLandArea = cityData.reduce( function(total, currentCity){
+    return total + currentCity.land_area   //the total is an accumulator. It starts at the set value and continues to log the total
+},0)                                         // as it iterates through the array
+//Starts the total at a certain number. The number can be anything
+console.log(totalLandArea)
 
 
 
